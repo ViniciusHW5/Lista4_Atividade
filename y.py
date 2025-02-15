f66a88e6ef38131a38d173b1f4628c808839be49
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-14 21:41:07 -0300

    Update y.py

--- y.py
+++ y.py
@@ -1,6 +1,11 @@
-nome_do_cliente = input("digite nome do cliente: ")
-Tele_do_cliente = input("digite Telefone do Cliente: ")
-Bairro_do_cliente = input ("Digite Bairro Do Cliente: ")
-print ("Nome:",nome_do_cliente)
-print ("Telefone:",Tele_do_cliente)
-print ("Bairro:",Bairro_do_cliente)
+nome_dos_cliente = ['Gabriel','João','Judas','Matheus','Vinicius','Seraphine','Niko']
+nome_dos_cliente = input('o nome do cliente em letra minuscula ')
+nome_dos_cliente.
+
+
+
+#Tele_dos_cliente = input("digite Telefone do Cliente: ")
+#Bairro_dos_cliente = input ("Digite Bairro Do Cliente: ")
+#print ("Nome:",nome_dos_cliente)
+#print ("Telefone:",Tele_dos_cliente)
+#print ("Bairro:",Bairro_dos_cliente)
