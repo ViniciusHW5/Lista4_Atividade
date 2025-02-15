d815d73043bb65c1c2b252839a5e8110517439b8
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-14 22:03:59 -0300

    falta tudo

--- y.py
+++ y.py
@@ -1,11 +1,26 @@
-nome_dos_cliente = ['Gabriel','João','Judas','Matheus','Vinicius','Seraphine','Niko']
-nome_dos_cliente = input('o nome do cliente em letra minuscula ')
-nome_dos_cliente.
 
 
 
-#Tele_dos_cliente = input("digite Telefone do Cliente: ")
-#Bairro_dos_cliente = input ("Digite Bairro Do Cliente: ")
-#print ("Nome:",nome_dos_cliente)
-#print ("Telefone:",Tele_dos_cliente)
-#print ("Bairro:",Bairro_dos_cliente)
+
+
+
+
+
+
+
+
+#nome_dos_cliente = ['Gabriel','João','Judas','Matheus','Seraphine','Niko']
+#nome_do_cliente = input('Digite o nome do cliente ')
+#nome_dos_cliente.append(nome_do_cliente)
+
+#Telefone_dos_Cliente = ['55-45-8887','55-34-6664','55-22-4446','55-13-2266','55-87-0088','55-65-9990']
+#Telefone_do_Cliente = input('Digite o telefone ')
+#Telefone_dos_Cliente.append(Telefone_do_Cliente)
+
+#Bairro_dos_cliente = ['GG','JJ','JJ','MM','SS','NN']
+#Bairro_do_cliente = input('Digite o Bairro do cliente ')
+#Bairro_dos_cliente.append(Bairro_do_cliente)
+
+#print (nome_dos_cliente)
+#print (Telefone_dos_Cliente)
+#print (Bairro_dos_cliente)
