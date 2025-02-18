9ae30e5ec7d9846d522fbd7ecd11abe8fead9462
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-18 19:58:33 -0300

    mudei

--- Ex2.py
+++ Ex2.py
@@ -8,4 +8,38 @@
 #maior ou igual 79 e menor 86 meio pesado
 #maior ou igual 90 e <100 pesado 
 #maior ou igual 100 pesado	
+nome = input(("qual nome do lutador? "))
+peso = float(input("qual o seu peso? "))
+if peso < (52):
+    print("invalido, tente de novo")
+
+elif peso >= (52) and peso< (65):
+    print(nome)
+    print("peso pena")
+
+elif peso >= (62) and peso < (72):
+    print(nome)
+    print("peso pena")
+
+elif peso >= (72) and peso < (79):
+    print(nome)
+    print("peso ligeiro")
+
+elif peso >= (79) and peso < (86):
+    print(nome)
+    print("peso meio medio")
+
+elif peso >= (86) and peso < (90):
+    print(nome)
+    print("peso medio")
+
+elif peso >= (90) and peso < (100):
+    print(nome)
+    print("meio pesado")
+
+elif peso >= (100):
+    print(nome)
+    print("peso pesado")
+
+
 
--- Ex5.py
+++ Ex5.py
@@ -2,3 +2,10 @@
 # Uma loja tem como política comercial aplicar margem bruta de 45% quando o preço de custo e menor ou igual a 100 reais se o 
 # produto custa mais do que isso e a margem bruta e 35%. Escreva um programa que leia o preço de custo do produto e mostre na
 #  tela seu preço de venda com duas 
+preco_custo =float(input("Digite o preço de custo do produto: "))
+if preco_custo <=100:
+    margem = 0.45
+else:
+    margem = 0.35
+preco_venda =preco_custo * (1+margem)
+print("O preço de venda do protudo é:R$",preco_venda)
