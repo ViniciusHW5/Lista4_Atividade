3aca9ed3f561b5ef5ceda27b382abc640a1afbe0
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-18 19:10:02 -0300

    atividade 1

--- Ex1.py
+++ Ex1.py
@@ -3,19 +3,18 @@
 #O grau de aceitação e o valor e a ser investido 
 #O grau de aceitação de risco deve ser lido no teclado na forma BX ou AL
 #Se fornecido algo diferente disso o programa deve mostra uma mensagem indicando que foi fornecido dado invalido. Para o valor dever ser numero real
-
-rend1 =float(print("quando você quer depositar? "))    
-aceita_risc1 = input ("qual a sua aceitação de risco, bx ou al? ").strip().lower()
-if aceita_risc1 == "bx" and rend1 < 1000.00:
-    print("você deve abrir uma poupança")
- if rend1 > 1000.00:
-
-#    print("você deve abrir uma renda fixa")
-#else aceita_risc1 == "al":
-#    rend2 =float(input("quanto vc quer depositar? "))
-#    if rend2 <(1000.00):
-#        print("voce deve investir em bitcoin. ")
-#    else:
-#        print("voce deve abrir uma ação")
-#else:
-#    print("dado informado invalido")
+aceita_risco = input ("qual a sua aceitação de risco, bx ou al? ").strip().lower()
+if aceita_risco == 'bx' or aceita_risco == 'al':
+    valor_investido =float(input("Quando voce quer depositar? "))
+    if valor_investido < 1000.00:
+        if aceita_risco == "bx":
+            print("voce deve abrir um poupança.")
+        else:
+            print("voce deve abrir uma renda fixa.")
+    elif aceita_risco == "al":
+        if valor_investido < 1000.00:   
+            print("voce deve investir em bitcoin")
+    else:
+        print("voce deve abrir uma ação.")
+if aceita_risco != "bx" and aceita_risco != "al":
+    print("Dado invalido tente novamente!")
